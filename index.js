d6286a4cd2d71b2e15b8ed0af02969d0329f7404
NikDoe <nikd0e@yandex.ru>
2022-07-13 15:47:31 +0300

    feature(e-comm): understanding form submissions

--- index.js
+++ index.js
@@ -5,14 +5,18 @@ const app = express();
 app.get('/', (request, response) => {
 	response.send(`
 	<div>
-		<form>
-			<input placeholder="email" />
-			<input placeholder="password" />
-			<input placeholder="password confirmation" />
+		<form method="post">
+			<input name="email" placeholder="email" />
+			<input name="password" placeholder="password" />
+			<input name="passwordConfirm" placeholder="password confirmation" />
 			<button>Sign Up</button>
 		</form>
 	</div>
 	`);
+});
+
+app.post('/', (request, response) => {
+	response.send('Account created !😁');
 });
 
 app.listen(3000, () => {
