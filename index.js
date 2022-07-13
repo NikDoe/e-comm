add3c82cc02e20f1b5504c6703f22fd235257c24
NikDoe <nikd0e@yandex.ru>
2022-07-13 09:32:14 +0300

    feature(e-comm): creating express server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hi There!:)');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+	response.send('Hi there!😉');
+});
+
+app.listen(3000, () => {
+	console.log('listening');
+});
